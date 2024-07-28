b810570d867a0ad5b4ecbfbb354f40a90c193261
yewon97 <dpdnjs9936@gmail.com>
2024-07-28 22:40:05 +0900

    home 페이지 게시글 리스트 만듬

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,11 +10,38 @@ export default function Home() {
           <Link to="/profile"> 프로필</Link>
         </div>
       </header>
-      <div className="post-list">post-list</div>
+      <div className="post__navigation">
+        <div className="post__navigation--active">전체</div>
+        <div>나의 글</div>
+      </div>
+      <div className="post__list">
+        {[...Array(10)].map((e, idx) => (
+          <div key={idx} className="post__box">
+            <Link to={`/posts/${idx}`}>
+              <div className="post__profile-box">
+                <div className="post__profile"></div>
+                <div className="post__author-name">패스트캠퍼스</div>
+                <div className="post__date">2023.08.09 토요일</div>
+              </div>
+              <div className="post__title">게시글 {idx}</div>
+              <div className="post__text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+                autem sit sequi similique unde officia eius consequatur, maxime
+                reiciendis incidunt odio aliquid soluta, quod, consequuntur
+                voluptatibus exercitationem quis aut obcaecati!
+              </div>
+              <div className="post__utils-box">
+                <div className="post__delete">삭제</div>
+                <div className="post__edit">수정</div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
       <footer>
-        <div>Menu 1</div>
-        <div>Menu 2</div>
-        <div>Menu 3</div>
+        <Link to="/posts/new">글쓰기</Link>
+        <Link to="/posts">게시글</Link>
+        <Link to="/profile"> 프로필</Link>
       </footer>
     </div>
   );
