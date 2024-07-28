bcb1297f5b7701b470ce79d6820c0f11be105eaa
yewon97 <dpdnjs9936@gmail.com>
2024-07-28 22:51:57 +0900

    post detail 컴포넌트 생성, header, footer 컴포넌트 분리

---
+++ src/components/PostDetail.tsx
@@ -0,0 +1,28 @@
+export default function PostDetail() {
+  return (
+    <>
+      <div className="post__detail">
+        <div className="post__box">
+          <div className="post__title">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non rem
+          </div>
+          <div className="post__profile-box">
+            <div className="post__profile"></div>
+            <div className="post__author-name">패스트캠퍼스</div>
+            <div className="post__date">2023.08.09 토요일</div>
+          </div>
+          <div className="post__utils-box">
+            <div className="post__delete">삭제</div>
+            <div className="post__edit">수정</div>
+          </div>
+          <div className="post__text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero autem
+            sit sequi similique unde officia eius consequatur, maxime reiciendis
+            incidunt odio aliquid soluta, quod, consequuntur voluptatibus
+            exercitationem quis aut obcaecati!
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -1,3 +1,13 @@
-export default function PostDetail() {
-  return <div>PostDetail Page</div>;
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import PostDetail from "../../components/PostDetail";
+
+export default function PostDetailPage() {
+  return (
+    <>
+      <Header />
+      <PostDetail />
+      <Footer />
+    </>
+  );
 }
