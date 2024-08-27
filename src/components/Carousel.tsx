0d28c9a6df87711f00156d202dbda6b22786d1b5
yewon97 <dpdnjs9936@gmail.com>
2024-08-27 22:22:52 +0900

    Add: 캐러셀 만들기

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,118 @@
+import { useState } from "react";
+
+const IMAGE_1_URL =
+  "https://images.unsplash.com/photo-1707343843437-caacff5cfa74?q=80&w=3475&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+const IMAGE_2_URL =
+  "https://images.unsplash.com/photo-1543227359-8c834db8028f?q=80&w=3348&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+const IMAGE_3_URL =
+  "https://images.unsplash.com/photo-1724406134629-15dd96ed6039?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+export default function Carousel() {
+  const [activeImage, setActiveImage] = useState(1);
+
+  return (
+    <div>
+      <div className="carousel">
+        <ul className="carousel__slides">
+          <input
+            type="radio"
+            name="radio-buttons"
+            id="img-1"
+            checked={activeImage === 1}
+            readOnly
+          />
+          <li className="carousel__slide-container">
+            <div className="carousel__slide-img">
+              <img src={IMAGE_1_URL} alt="scenery 1" />
+            </div>
+            <div className="carousel__controls">
+              <label
+                onClick={() => setActiveImage(3)}
+                className="carousel__slide-prev"
+              >
+                <span>&lsaquo;</span>
+              </label>
+              <label
+                onClick={() => setActiveImage(2)}
+                className="carousel__slide-next"
+              >
+                <span>&rsaquo;</span>
+              </label>
+            </div>
+          </li>
+          <input
+            type="radio"
+            name="radio-buttons"
+            id="img-2"
+            checked={activeImage === 2}
+            readOnly
+          />
+          <li className="carousel__slide-container">
+            <div className="carousel__slide-img">
+              <img src={IMAGE_2_URL} alt="scenery 2" />
+            </div>
+            <div className="carousel__controls">
+              <label
+                onClick={() => setActiveImage(1)}
+                className="carousel__slide-prev"
+              >
+                <span>&lsaquo;</span>
+              </label>
+              <label
+                onClick={() => setActiveImage(3)}
+                className="carousel__slide-next"
+              >
+                <span>&rsaquo;</span>
+              </label>
+            </div>
+          </li>
+          <input
+            type="radio"
+            name="radio-buttons"
+            id="img-3"
+            checked={activeImage === 3}
+            readOnly
+          />
+          <li className="carousel__slide-container">
+            <div className="carousel__slide-img">
+              <img src={IMAGE_3_URL} alt="scenery 3" />
+            </div>
+            <div className="carousel__controls">
+              <label
+                onClick={() => setActiveImage(2)}
+                className="carousel__slide-prev"
+              >
+                <span>&lsaquo;</span>
+              </label>
+              <label
+                onClick={() => setActiveImage(1)}
+                className="carousel__slide-next"
+              >
+                <span>&rsaquo;</span>
+              </label>
+            </div>
+          </li>
+          <div className="carousel__dots">
+            <label
+              onClick={() => setActiveImage(1)}
+              className="carousel__dot"
+              id="img-dot-1"
+            ></label>
+            <label
+              onClick={() => setActiveImage(2)}
+              className="carousel__dot"
+              id="img-dot-2"
+            ></label>
+            <label
+              onClick={() => setActiveImage(3)}
+              className="carousel__dot"
+              id="img-dot-3"
+            ></label>
+          </div>
+        </ul>
+      </div>
+    </div>
+  );
+}
