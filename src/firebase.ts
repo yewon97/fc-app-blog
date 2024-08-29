b2688ea0e24788d7c8557ed466a7f02038e3d230
yewon97 <dpdnjs9936@gmail.com>
2024-08-29 22:08:41 +0900

    firebase 셋팅

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBCacK8alFTzQUcn4nm4BzyDS530WNvH0Q",
+  authDomain: "fc-blog-app.firebaseapp.com",
+  projectId: "fc-blog-app",
+  storageBucket: "fc-blog-app.appspot.com",
+  messagingSenderId: "784182758547",
+  appId: "1:784182758547:web:38de8ac8cb619901285166",
+  measurementId: "G-R4QZ5XKYQ0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
