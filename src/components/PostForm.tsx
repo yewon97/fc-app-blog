7a98b7b9c4af41a635ff235beda6b151a140b162
yewon97 <dpdnjs9936@gmail.com>
2024-09-09 21:24:32 +0900

    firebase store 연동해서 컬렉션에 데이터 저장하기

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -1,6 +1,56 @@
+import { AuthContext } from "@/context/AuthContext";
+import { db } from "@/firebaseApp";
+import { addDoc, collection } from "firebase/firestore";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+type PostFormProps = {
+  title: string;
+  summary: string;
+  content: string;
+};
+
 export default function PostForm() {
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const [postForm, setPostForm] = useState<PostFormProps>({
+    title: "",
+    summary: "",
+    content: "",
+  });
+
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = e.target;
+    setPostForm({ ...postForm, [name]: value });
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await addDoc(collection(db, "posts"), {
+        ...postForm,
+        createAt: new Date()?.toLocaleDateString(),
+        email: user?.email,
+      });
+      toast.success("게시글이 성공적으로 작성되었습니다.");
+      navigate("/");
+    } catch (error: any) {
+      console.error(error);
+      toast.error("게시글 작성에 실패했습니다.");
+    }
+  };
+
+  useEffect(() => {
+    console.log(postForm);
+  }, [postForm]);
+
   return (
-    <form action="/post" method="POST" className="form">
+    <form className="form" onSubmit={onSubmit}>
       <div className="form__block">
         <label htmlFor="title">제목</label>
         <input
@@ -9,6 +59,7 @@ export default function PostForm() {
           id="title"
           required
           placeholder="제목을 입력해주세요."
+          onChange={onChange}
         />
       </div>
       <div className="form__block">
@@ -19,6 +70,7 @@ export default function PostForm() {
           id="summary"
           required
           placeholder="요약을 입력해주세요."
+          onChange={onChange}
         />
       </div>
       <div className="form__block">
@@ -28,6 +80,7 @@ export default function PostForm() {
           id="content"
           required
           placeholder="내용을 입력해주세요."
+          onChange={onChange}
         />
       </div>
       <div className="form__block">
